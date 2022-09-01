3c5010d5738e079e5f258c4d3df481be48aa6cb8
TemesgeA <temesgen.ayneka@gmail.com>
2022-09-01 14:47:59 +0300

    add popup class to render modal

---
+++ src/modules/popupReservation.js
@@ -0,0 +1,44 @@
+renderPopup(data) {
+    const res = data.meals[0];
+
+    const id = res.idMeal;
+    const thumb = res.strMealThumb;
+    const name = res.strMeal;
+    const category = res.strCategory;
+    const area = res.strArea;
+    const recipie = res.strSource;
+
+    const popup = document.getElementById('reservation__data-content');
+    this.removePopup();
+
+    let html = `
+    <img src='${thumb}' alt="Image of ${name}">
+    <h2>${name}</h2>
+    <div id='resPopup--dish-description'>
+      <div class='rpdd-item'>
+        <p>Category:</p>
+        <p>${category}</p>
+      </div>
+      <div class='rpdd-item'>
+        <p>Origin:</p>
+        <p>${area}</p>
+      </div>
+      <div class='rpdd-item'>
+        <a href='${recipie}' target="_blank">Recipie</a>
+        <i class="fa-solid fa-arrow-up-right-from-square"></i>
+      </div>
+    </div>
+    `;
+    html += this.AddShowRes.renderForm();
+    this.AddShowRes.getReservations(id);
+    popup.insertAdjacentHTML('afterbegin', html);
+
+    const form = document.getElementById('submit-reservation');
+    form.addEventListener('submit', (e) => {
+      const formData = new FormData(e.target);
+
+      e.preventDefault();
+      this.AddShowRes.submitForm(formData, e.target, id);
+      form.reset();
+    });
+  }
