c60026fb118964ba0c79e8c83f97c29798052fd5
techEmprez <choiceosobor@gmail.com>
2022-09-03 17:15:00 +0100

    add likes counter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import './style.css';
 import getData from './modules/get.js';
+import getLikes from './modules/getLikes.js';
+import addEventToLike from './modules/showLikes.js';
+import imgUrl from './asset/like-svgrepo-com.svg';
+
 // console.log(getData());
+addEventToLike();
 getData().then((data) => {
   const listOfMeals = data.meals;
   listOfMeals.forEach((item) => {
@@ -12,7 +17,14 @@ getData().then((data) => {
       <p>${item.strInstructions}</p>
       <button type="button" class="comments-btn1">Comments</button>
       <button type="button" class="comments-btn2">reservations</button>
+      <div class="like">
+      <img src='${imgUrl}' class="icon" id='${item.idMeal}' />          
+      <span class="qty">0</span> <span>Likes</span></p>
+      </div>
     `;
+
     document.querySelector('.items-container').appendChild(meal);
   });
+  addEventToLike();
+  getLikes();
 });
