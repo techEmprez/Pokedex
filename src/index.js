2f61e0b8daa53451ef1b8d7e83e441dffc79d73e
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-09-03 17:07:43 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,29 +4,32 @@ import getMealData from './modules/get.js';
 import display, { getComment, post } from './modules/getcomapi.js';
 getMealData().then((data) => {
   const listOfMeals = data.meals;
-  listOfMeals.forEach((item) => {
+  listOfMeals.forEach((item, id) => {
     const meal = document.createElement('div');
     meal.classList.add('meal');
     meal.innerHTML = `
       <h2>${item.strMeal}</h2>
       <img src="${item.strMealThumb}" alt="${item.strMeal}">
-      
-      <button type="button" class="comments-btn1">Comments</button>
+
+      <button type="button" class="comments-btn1" id="comment-btn-${id}" data-bs-toggle="modal" data-bs-target="#exampleModal">Comments</button>
       <button type="button" class="reservations-btn">reservations</button>
 
     `;
     document.querySelector('.items-container').appendChild(meal);
+    const commentBtns = document.querySelectorAll(`.comments-btn1`);
+
+    commentBtns.forEach((item) => {
+      item.addEventListener('click', (e) => {
+        const commentId = +e.target.id.split('-')[2];
+        if (commentId === id) {
+          display(commentId);
+          getComment(commentId);
+        }
+      });
+    });
   });
 });
 
-window.addEventListener('click', (e) => {
-  if (!e.target.matches('comments-btn1')) return;
-  if (e.target.parentElement.id === e.target.id) {
-    const x = e.target.id;
-    display(x);
-    getComment(x);
-  }
-});
 window.addEventListener('submit', (e) => {
   e.preventDefault();
   if (!e.target.matches('.form')) return;
